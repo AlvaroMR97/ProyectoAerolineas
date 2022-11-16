c15c03b4bcc1cde5fd3797b31b4d5ae00d66b805
David López Chica <David.LopezChica@solera.com>
2022-11-16 10:08:35 +0100

    Add initial classes for data containers

---
+++ src/main/java/com/solera/bootcamp/airlinemanager/airlinemanager/FlightBean.java
@@ -0,0 +1,75 @@
+package com.solera.bootcamp.airlinemanager.airlinemanager;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FlightBean {
+	private String airline;
+	private String origin_city;
+	private String destination_city;
+	private ArrayList<String> scales;
+	private boolean luggage_allowed;
+	private Date flight_date;
+
+	public FlightBean(String airline, String origin_city, String destination_city, ArrayList<String> scales,
+			boolean luggage_allowed, Date flight_date) {
+		super();
+		this.airline = airline;
+		this.origin_city = origin_city;
+		this.destination_city = destination_city;
+		this.scales = scales;
+		this.luggage_allowed = luggage_allowed;
+		this.flight_date = flight_date;
+	}
+
+	public String getAirline() {
+		return airline;
+	}
+
+	public void setAirline(String airline) {
+		this.airline = airline;
+	}
+
+	public String getOrigin_city() {
+		return origin_city;
+	}
+
+	public void setOrigin_city(String origin_city) {
+		this.origin_city = origin_city;
+	}
+
+	public String getDestination_city() {
+		return destination_city;
+	}
+
+	public void setDestination_city(String destination_city) {
+		this.destination_city = destination_city;
+	}
+
+	public ArrayList<String> getScales() {
+		return scales;
+	}
+
+	public void setScales(ArrayList<String> scales) {
+		this.scales = scales;
+	}
+
+	public boolean isLuggage_allowed() {
+		return luggage_allowed;
+	}
+
+	public void setLuggage_allowed(boolean luggage_allowed) {
+		this.luggage_allowed = luggage_allowed;
+	}
+
+	public Date getFlight_date() {
+		return flight_date;
+	}
+
+	public void setFlight_date(Date flight_date) {
+		this.flight_date = flight_date;
+	}
+}
---
+++ src/main/java/com/solera/bootcamp/airlinemanager/airlinemanager/PassengerBean.java
@@ -0,0 +1,61 @@
+package com.solera.bootcamp.airlinemanager.airlinemanager;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PassengerBean {
+	private String name;
+	private String surname;
+	private String nationality;
+	private String idDocument;
+	private int age;
+
+	public PassengerBean(String name, String surname, String nationality, String idDocument, int age) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.nationality = nationality;
+		this.idDocument = idDocument;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String getIdDocument() {
+		return idDocument;
+	}
+
+	public void setIdDocument(String idDocument) {
+		this.idDocument = idDocument;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
