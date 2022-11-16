8e0cf7647438b182ebda7fef8e371ca8c1b1a8a0
David López Chica <David.LopezChica@solera.com>
2022-11-16 09:51:26 +0100

    Initial commit

---
+++ src/main/java/com/solera/bootcamp/airlinemanager/airlinemanager/AirlineManagerApplication.java
@@ -0,0 +1,13 @@
+package com.solera.bootcamp.airlinemanager.airlinemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineManagerApplication.class, args);
+	}
+
+}
